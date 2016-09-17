827445a6529f479b93289f69c8a946b1b27c5594
IshaDate1 <tekgeek9@gmail.com>
2016-09-17 15:12:56 -0700

    first commit

---
+++ ballshooter.cpp
@@ -0,0 +1,18 @@
+/*
+ * ballshooter.cpp
+ *
+ *  Created on: Sep 17, 2016
+ *      Author: mayur
+ */
+
+#include "ballshooter.h"
+
+ballshooter::ballshooter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ballshooter::~ballshooter() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ballshooter.h
@@ -0,0 +1,17 @@
+/*
+ * ballshooter.h
+ *
+ *  Created on: Sep 17, 2016
+ *      Author: mayur
+ */
+
+#ifndef BALLSHOOTER_H_
+#define BALLSHOOTER_H_
+
+class ballshooter {
+public:
+	ballshooter();
+	virtual ~ballshooter();
+};
+
+#endif /* BALLSHOOTER_H_ */
